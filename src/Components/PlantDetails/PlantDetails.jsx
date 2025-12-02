395a369927ba76db0eaadb2eb6b16db6aecbc4d1
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-12-02 10:05:48 +0600

    Updated Plants Section

--- src/Components/PlantDetails/PlantDetails.jsx
+++ src/Components/PlantDetails/PlantDetails.jsx
@@ -44,37 +44,39 @@ const PlantDetails = ({ dataPromise }) => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-5 md:gap-y-0 items-center p-20">
-      <img src={plant.image} alt={plant.plantName} className="w-60 md:w-80 h-full object-cover rounded-lg mb-5" />
-      <div className='w-60 md:w-80'>
-        <h1 className="text-4xl font-black mb-3">{plant.plantName}</h1>
-        <p className="mb-3 text-lg text-justify">{plant.description}</p>
-        <p><span className='font-bold'>Watering:</span> {plant.watering}</p>
-        <p><span className='font-bold'>Sunlight:</span> {plant.sunlight}</p>
-        <p><span className='font-bold'>Fertilizing:</span> {plant.fertilizing}</p>
-        <p><span className='font-bold'>Price:</span> {plant.price}</p>
-        <p><span className='font-bold'>Rating:</span> {plant.rating}</p>
-        <p><span className='font-bold'>Care Level:</span> {plant.careLevel}</p>
-        <p><span className='font-bold'>Provider:</span> {plant.providerName}</p>
-        <p><span className='font-bold'>Stock:</span> {plant.availableStock}</p>
-      </div>
+    // <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-5 md:gap-y-0 items-center p-20">
+    //   <img src={plant.image} alt={plant.plantName} className="w-60 md:w-80 h-full object-cover rounded-lg mb-5" />
+    //   <div className='w-60 md:w-80'>
+    //     <h1 className="text-4xl font-black mb-3">{plant.plantName}</h1>
+    //     <p className="mb-3 text-lg text-justify">{plant.description}</p>
+    //     <p><span className='font-bold'>Watering:</span> {plant.watering}</p>
+    //     <p><span className='font-bold'>Sunlight:</span> {plant.sunlight}</p>
+    //     <p><span className='font-bold'>Fertilizing:</span> {plant.fertilizing}</p>
+    //     <p><span className='font-bold'>Price:</span> {plant.price}</p>
+    //     <p><span className='font-bold'>Rating:</span> {plant.rating}</p>
+    //     <p><span className='font-bold'>Care Level:</span> {plant.careLevel}</p>
+    //     <p><span className='font-bold'>Provider:</span> {plant.providerName}</p>
+    //     <p><span className='font-bold'>Stock:</span> {plant.availableStock}</p>
+    //   </div>
 
-      <div className="card bg-base-100 w-60 md:w-80 max-w-sm shrink-0 shadow-2xl border border-emerald-700">
-        <form onSubmit={handleSubmit} className="card-body">
-          <h1 className='text-center text-xl font-bold bg-linear-to-r from-emerald-700 to-lime-600 bg-clip-text text-transparent'>Book Consultation</h1>
-          <fieldset className="fieldset">
-            <label className="label">Name</label>
-            <input type="text" className="input" value={formData.name}
-              onChange={handleChange} placeholder="Name" />
-            <label className="label">Email</label>
-            <input type="email" className="input" value={formData.email}
-              onChange={handleChange} placeholder="Email" />
-            <button type='submit' className="btn mt-4 bg-linear-to-r from-emerald-700 to-lime-600 text-white">Book Now</button>
-          </fieldset>
-        </form>
-      </div>
+    //   <div className="card bg-base-100 w-60 md:w-80 max-w-sm shrink-0 shadow-2xl border border-emerald-700">
+    //     <form onSubmit={handleSubmit} className="card-body">
+    //       <h1 className='text-center text-xl font-bold bg-linear-to-r from-emerald-700 to-lime-600 bg-clip-text text-transparent'>Book Consultation</h1>
+    //       <fieldset className="fieldset">
+    //         <label className="label">Name</label>
+    //         <input type="text" className="input" value={formData.name}
+    //           onChange={handleChange} placeholder="Name" />
+    //         <label className="label">Email</label>
+    //         <input type="email" className="input" value={formData.email}
+    //           onChange={handleChange} placeholder="Email" />
+    //         <button type='submit' className="btn mt-4 bg-linear-to-r from-emerald-700 to-lime-600 text-white">Book Now</button>
+    //       </fieldset>
+    //     </form>
+    //   </div>
+    // </div>
+    <div>
+
     </div>
-
   );
 };
 
--- src/Components/Plants/Plants.jsx
+++ src/Components/Plants/Plants.jsx
@@ -11,25 +11,6 @@ const Plants = ({ dataPromise }) => {
     navigate(`/plants/${plantId}`);
   }
   return (
-    // <div className='max-w-7xl mx-auto pt-10 pb-40'>
-    //   <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-10 px-8 md:px-0'>
-    //     {
-    //       data.map(toprated =>
-    //         <div className='border border-gray-300 mb-5 md:mb-0 rounded-xl hover:scale-102 transition'>
-    //           <div className='p-5'>
-    //             <img className='h-96 w-full object-cover rounded-tr-md rounded-tl-md' src={toprated.image} alt="" />
-    //             <p className='font-bold text-xl py-4'>{toprated.plantName}</p>
-    //             <div className='flex justify-between items-center pb-5'>
-    //               <span className='flex items-center text-xs badge text-white bg-emerald-700 font-medium gap-x-2'><FaCreditCard /> {toprated.price}</span>
-    //               <span className='flex items-center text-xs badge text-white bg-lime-600 font-medium gap-x-2'><IoIosStar /> {toprated.rating}</span>
-    //             </div>
-    //             <button onClick={() => handleDetails(toprated.plantId) } className='btn w-full bg-linear-to-r from-emerald-700 to-lime-600 text-white'>View Details</button>
-    //           </div>
-    //         </div>
-    //       )
-    //     }
-    //   </div>
-    // </div>
     <div className='max-w-7xl mx-auto py-30'>
       <h1 className='font-bold text-xl md:text-2xl text-center'>All Plants: {data.length}</h1>
       <div className='grid grid-cols-2 md:grid-cols-4 gap-10 py-10 px-8 md:px-0'>
